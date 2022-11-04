c9c22247e073aa85d36cc8fa5fe09daa7234e12c
norman-andrians <norman25.na@gmail.com>
2022-11-04 22:37:37 +0700

    perbarui form konfirmasi email di pendaftaran

--- assets/scripts/form/form-manajement.js
+++ assets/scripts/form/form-manajement.js
@@ -1,5 +1,13 @@
 const inptext = document.querySelectorAll(".inp-text");
+const inpOtp = document.querySelector(".inp-six-dig").children;
 
+const keycodes = {
+    "backspace" : 8,
+    "right" : 39,
+    "left" : 37
+}
+
+const nums = Array.from(Array(10).keys());
 
 // Fungsi yang mendeteksi masukan input apakah kosong atau tidak
 function checkInput() {
@@ -59,10 +67,34 @@ function checkInput() {
 
 $(document).ready(() => {
     checkInput();
-
     for (let i = 0; i < inptext.length; i++) {
         inptext[i].children[1].addEventListener("focusin", () => { inptext[i].children[0].style.width = "100%"; });
         inptext[i].children[1].addEventListener("focusout", () => { inptext[i].children[0].style.width = "0%"; });
         inptext[i].children[1].addEventListener("input", () => { checkInput(); });
+    }
+
+    for (let i = 0; i < inpOtp.length; i++) {
+        inpOtp[i].addEventListener('input', () => {
+            inpOtp[i].value = inpOtp[i].value.length > inpOtp[i].maxLength ? inpOtp[i].value.slice(0, inpOtp[i].maxLength) : inpOtp[i].value;
+        });
+        inpOtp[i].addEventListener('keyup', (ev) => {
+            if (i < inpOtp.length - 1 && ev.keyCode != keycodes.backspace) {
+                for (let b = 0; b < nums.length; b++) { 
+                    if (parseInt(ev.key) === nums[b] && inpOtp[i].value != '') {
+                        inpOtp[i+1].focus();
+                        break;
+                    }
+                }
+            }
+        });
+        
+        inpOtp[i].addEventListener('keydown', (ev) => {
+            if (i < inpOtp.length - 1 && ev.keyCode === keycodes.right) {
+                inpOtp[i+1].focus();
+            }
+            else if (i > 0 && ev.keyCode === keycodes.left) {
+                inpOtp[i-1].focus();
+            }
+        });
     }
 });
