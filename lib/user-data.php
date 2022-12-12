bbf8ab26c089a5ba3245057bd06bcf1a8899b455
norman-andrians <norman25.na@gmail.com>
2022-12-12 19:21:29 +0700

    update user data

--- lib/user-data.php
+++ lib/user-data.php
@@ -1,4 +1,17 @@
 <?php
+// ===
+// *
+// * Library user data kelas-kelas dan kueri mysqli berorientasi objek dasar
+// * dengan sintaks kode sederhana
+// * Berisi:
+// * 1. Kelas User yang terdiri dari:
+// *    - Properti id, uername, nickname, email, prioritas, password dan id bisnis
+// *    - Method SetPassword, GetUsername dan CRUD
+// * 2. Kelas Bisnis yang terdiri dari:
+// *
+// *
+// ===
+
 include '../connect.php';
 
 class User {
@@ -7,21 +20,42 @@ class User {
     public $nickname;
     public $email;
     public $priority;
+
     protected $password;
     protected $id_business;
 
-    public function __construct($id, $username, $nickname, $email, $password, $priority) {
+    // Construktor hanya terdiri dari 'id', 'username', dan 'email' untuk entry data dalam kelas dasar
+    // Bisa disebut juga data sementara, ini tidak disimpan dalam cookie
+    // * id business tidak boleh di konstruktor karena kelas business dulu yang harus dibuat
+    public function __construct($id, $username, $email) {
         $this->id = $id;
         $this->username = $username;
-        $this->nickname = $nickname;
         $this->email = $email;
-        $this->priority = $priority;
-        $this->password = $password;
     }
 
     // Mengubah Password
     public function SetPassword($password) {
         $this->password = $password;
+    }
+
+    public function GetId() {
+        include '../connect.php';
+
+        $id = "";
+        $username = $this->username;
+
+        $query = "SELECT `username` FROM `users` WHERE `username` = '$username'";
+        $result = $connect->query($query);
+        
+        if ($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            $id = $row['id'];
+        }
+        else {
+            // Error: id pengguna tidak ditemukan
+        }
+        
+        return $id;
     }
 
     // Mendapatkan username sesuai objek ini dari database
@@ -43,19 +77,21 @@ class User {
     }
 
     // Membuat data user
-    public function Create() {
+    public function Create($nickname, $priority, $pw, $id_business) {
         include '../connect.php';
 
+        $this->nickname = $nickname;
+        $this->priority = $priority;
+        $this->password = $pw;
+        $this->id_business = $id_business;
+
         $username = $this->username;
-        $nickname = $this->nickname;
         $email = $this->email;
-        $priority = $this->priority;
-        $password = $this->password;
-        $id_business = $this->id_business;
 
         if ($this->GetUsername() != "$username") {
-            $query = "INSERT `users` (`id`, `username`, `nickname`, `email`, `prioritas`, `password`, `id_business`) VALUES (NULL, '$username', '$nickname', '$email', '$priority', '$password', '$id_business')";
-            $connect->query($query);
+            $query = "INSERT `users` (`id`, `username`, `nickname`, `email`, `prioritas`, `password`, `id_business`) VALUES (NULL, '$username', '$nickname', '$email', '$priority', '$pw', '$id_business')";
+
+            
         }
         else {
             // Error: `username` sudah tersedia
@@ -66,20 +102,70 @@ class User {
     public function Read() {
         include '../connect.php';
 
-        $query = "SELECT * FROM `users` WHERE `id` = 1";
+        $id = $this->id;
+
+        $query = "SELECT * FROM `users` WHERE `id` = $id";
         $result = $connect->query($query);
 
         return $result;
     }
 
-    public function Delete() {
-        include '..connect.php';
+    public function Update() {
+        include '../connect.php';
+
+        $id = $this->id;
+        $username = $this->username;
+        $nickname = $this->nickname;
+        $email = $this->email;
+        $priority = $this->priority;
+    }
+
+    // Memuat/update data dari database
+    public function Load() {
+        include '../connect.php';
+
+        $id = $this->id;
+
+        $query = "SELECT * FROM `users` WHERE `id` = $id";
+        $result = $connect->query($query);
+
+        if ($result->num_rows == 1) {
+            $rows = $result->fetch_assoc();
+
+            $this->username = $rows['username'];
+            $this->nickname = $rows['nickname'];
+            $this->email = $rows['email'];
+            $this->priority = $rows['prioritas'];
+            $this->password = $rows['password'];
+            $this->id_business = $rows['id_business'];
+        }
+        else {
+            // Error: data pengguna tidak ditemukan
+        }
+    }
+
+    // Menghapus data user dari database (Dimustahilkan private karena jejak digital)
+    // Jangan hapus data user yh
+    private function Delete() {
+        include '../connect.php';
 
         $username = $this->username;
         $query = "DELETE FROM `users` WHERE `username` = $username";
         $connect->query($query);
     }
 }
+
+$userd = new User(null, "erand", "ernad@example.com");
+$userd->Create("erand333", 0, "ender1234", 8);
+
+$data = $userd->Read();
+$r = $data->fetch_assoc();
+
+
+echo "id: ".$r['id']."<br>";
+echo "username: ".$r['username']."<br>";
+echo "nickname: ".$r['nickname']."<br>";
+echo "email: ".$r['email']."<br>";
 
 class Business extends User {
     public $b_id;
